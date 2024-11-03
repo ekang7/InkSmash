d34bc6499862594608d036f81910c7346823289a
Edward Kang <44958279+ekang7@users.noreply.github.com>
2024-11-03 04:55:32 -0500

    prompt engineering to include images more, descriptions, strengths and weaknesses

--- oai_api/ability_prompts.ts
+++ oai_api/ability_prompts.ts
@@ -4,6 +4,8 @@ export const SYSTEM_PROMPT = "You are an expert dungeon master, known for choreo
   "You will first help them name and describe their moves before the battle unfolds. ";
 
 export const MESSAGE_PROMPT = "Player 1 has submitted a drawing of their character and a piece of equipment. " +
-  "Please provide a name and description for the character's move with this equipment.";
+  "Please provide a name and description for the character's move with this equipment. Note what things this move is "
+  + "extremely effective against. Also note what things this move is extremely weak against. Please give examples and "
+  + "reasons for both";
 
 export const JSON_FORMAT = `{"name": "move name", "description": "move description"}`;
--- oai_api/fight_prompts.ts
+++ oai_api/fight_prompts.ts
@@ -7,10 +7,12 @@ export const MESSAGE_PROMPT = "Player 1's stats: {player_1_stats}.\n" +
   "Player 2's stats: {player_2_stats}.\n" +
   "Player 1's ability used: {player_1_move}.\n" +
   "Player 2's ability used: {player_2_move}.\n" +
-  "Attached below are also images of the players' characters and their abilities. " +
+  "Attached below are also images of the players' characters and their abilities. Player 1 is drawn by image_0.png and Player 1 uses " +
+  "the ability described in image_3.png against Player 2. Player 2 is drawn by image_1.png and Player 2 uses the ability described in " +
+  "image_4.png against Player 1. " +
   "Please provide the amount of damage each player takes and a short text description of the turn." +
   "Don't make the damage too high or too low, and make sure the description is engaging and creative." +
-  "To make sure the damage is balanced, the damage should be between -20-20. It can be negative if the ability heals the player.";
+  "To make sure the damage is balanced, the damage should be between the integer values of -20 through 20. It can be negative if the ability heals the player.";
 
 export const JSON_FORMAT = `{"player_1_dmg": 0, "player_2_dmg": 0,
   "description": "{player_1} jumps in the air and uses {player_1_move} to attack {player_2}, who counters with {player_2_move}."}`
